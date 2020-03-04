5572c6cc9236a4b704e1df457f502561f0446e8a
Carlos <cpinelly@gmail.com>
2020-03-03 21:37:18 -0500

    Empty message for favorites screen.

--- lib/screens/favorites_list.dart
+++ lib/screens/favorites_list.dart
@@ -9,11 +9,24 @@ class FavoritesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<Quotes>(
-      builder: (ctx, provider, _) => ListView.builder(
-        itemCount: (provider != null) ? provider.savedQuotes.length : 0,
-        itemBuilder: (context, index) =>
-            QuoteListItem(provider.savedQuotes[index]),
-      ),
+      builder: (ctx, provider, _) => provider.savedQuotes == null ||
+              provider.savedQuotes.length == 0
+          ? Center(
+              child: Container(
+              height: 100,
+              child: Column(
+                children: <Widget>[
+                  Text('Mark some quotes as favorites.', style: TextStyle(color: Colors.grey),),
+                  SizedBox(height: 12,),
+                  Icon(Icons.star, color: Colors.grey, size: 48,)
+                ],
+              ),
+            ))
+          : ListView.builder(
+              itemCount: (provider != null) ? provider.savedQuotes.length : 0,
+              itemBuilder: (context, index) =>
+                  QuoteListItem(provider.savedQuotes[index]),
+            ),
     );
   }
 }
