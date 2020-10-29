e617ff48f90ab54b008ac0f4dc0b64561780cfd3
Carlos <cpinelly@gmail.com>
2020-10-29 09:43:14 -0500

    Disabled white theme.

--- lib/models/QuoteTheme.dart
+++ lib/models/QuoteTheme.dart
@@ -15,7 +15,7 @@ class QuoteTheme {
     //QuoteTheme(id: 'amber', textColor: Colors.black , backgroundColor: Colors.amber),
     QuoteTheme(id: 'orange', textColor: Colors.white , backgroundColor: Colors.orange, secondaryColor: Colors.orange.shade600),
     QuoteTheme(id: 'purple', textColor: Colors.white , backgroundColor: Colors.purple, secondaryColor: Colors.purple.shade600),
-    QuoteTheme(id: 'white', textColor: Colors.black54 , backgroundColor: Colors.white, secondaryColor: Colors.black12),
+    //QuoteTheme(id: 'white', textColor: Colors.black54 , backgroundColor: Colors.white, secondaryColor: Colors.black12),
     QuoteTheme(id: 'pink', textColor: Colors.white , backgroundColor: Colors.pink, secondaryColor: Colors.pink.shade600),
     QuoteTheme(id: 'teal', textColor: Colors.white , backgroundColor: Colors.teal, secondaryColor: Colors.teal.shade600),
     //QuoteTheme(id: 'lime', textColor: Colors.black , backgroundColor: Colors.lime, secondaryColor: Colors.lime.shade600),
