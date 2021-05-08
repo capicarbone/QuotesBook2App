9ebcca78c28b305eaceabadbaf33f4ce1a9ec91b
Carlos Pinelly <cpinelly@gmail.com>
2021-05-07 22:24:43 -0500

    Added test logic for image generation.

--- lib/widgets/quote_listitem.dart
+++ lib/widgets/quote_listitem.dart
@@ -1,3 +1,5 @@
+import 'dart:developer' as developer;
+import 'dart:typed_data';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -17,17 +19,27 @@ import '../models/Quote.dart';
 import '../providers/saved_quotes.dart';
 import 'package:esys_flutter_share/esys_flutter_share.dart';
 
-class QuoteListItem extends StatelessWidget {
+class QuoteListItem extends StatefulWidget {
   Quote quote;
   Function onTap;
-  GlobalKey _repaintBoundaryKey = GlobalKey();
-
-  var loading = false;
 
   QuoteListItem({this.quote, this.onTap});
 
+  @override
+  _QuoteListItemState createState() => _QuoteListItemState();
+}
+
+class _QuoteListItemState extends State<QuoteListItem> {
+  GlobalKey _repaintBoundaryKey = GlobalKey();
+
+  final _shareDebugMode = true;
+
+  Uint8List _memoryImage;
+
+  var loading = false;
+
   void _onTextSharePressed() {
-    Share.text('A quote from Quotesbook', quote.toText(), 'text/plain');
+    Share.text('A quote from Quotesbook', widget.quote.toText(), 'text/plain');
   }
 
   void _toggleLoader(bool enabled) {}
@@ -48,24 +60,26 @@ class QuoteListItem extends StatelessWidget {
       rootBundle.load('assets/quote-logo.png').then((footerImage) {
         return compute(generator.generateQuoteImage, {
           'quoteImage': quoteImage,
-          'backgroundColor':
-              QuoteTheme.getThemeById(quote.themeId).backgroundColor,
+          'backgroundColor': Colors.white,
           'footerLogo': image.decodePng(footerImage.buffer.asUint8List())
         });
       }).then((generatedImage) {
         _toggleLoader(false);
 
+        /*
         Share.file("A Quote from Quotesbook", 'quote.jpg', generatedImage,
             'image/jpg');
-
-        /*
-        if (_debugMode){
+        */
+        if (_shareDebugMode){
           setState(() {
             _memoryImage = generatedImage;
           });
         }
-         */
-      }).catchError(() => _toggleLoader(false));
+
+      }).catchError((err) {
+        developer.log("on image generation", error: err);
+        _toggleLoader(false);
+      });
     });
   }
 
@@ -104,48 +118,46 @@ class QuoteListItem extends StatelessWidget {
 
   Widget _buildBottomMenu(BuildContext ctx) {
     final quotesProvider = Provider.of<SavedQuotes>(ctx, listen: false);
-    final saved = quotesProvider.isSaved(quote.id);
+    final saved = quotesProvider.isSaved(widget.quote.id);
 
-    var buttonsStyle =
-        TextButton.styleFrom(primary: Theme.of(ctx).primaryColor, textStyle: GoogleFonts.montserrat());
+    var buttonsStyle = TextButton.styleFrom(
+        primary: Theme.of(ctx).primaryColor,
+        textStyle: GoogleFonts.montserrat());
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
         if (saved)
-        TextButton(
-            style: buttonsStyle,
-            child: Text("Remove".toUpperCase()),
-            onPressed: () {
-              quotesProvider.removeQuote(quote);
-            }),
+          TextButton(
+              style: buttonsStyle,
+              child: Text("Remove".toUpperCase()),
+              onPressed: () {
+                quotesProvider.removeQuote(widget.quote);
+              }),
         if (!saved)
-        TextButton(
-          onPressed: () {
-            quotesProvider.saveQuote(quote);
-          },
-          child: Text("Save".toUpperCase()),
-          style: buttonsStyle,
-        ),
-
+          TextButton(
+            onPressed: () {
+              quotesProvider.saveQuote(widget.quote);
+            },
+            child: Text("Save".toUpperCase()),
+            style: buttonsStyle,
+          ),
         TextButton(
             style: buttonsStyle,
             child: Text("Share".toUpperCase()),
             onPressed: () {
               _onSharePressed(ctx);
             }),
-
       ],
     );
   }
 
   @override
   Widget build(BuildContext context) {
-
-    return Container(
+    final listItem = Container(
       width: double.infinity,
       child: GestureDetector(
-        onTap: onTap,
+        onTap: widget.onTap,
         child: Material(
           color: Colors.transparent,
           child: Stack(
@@ -182,7 +194,7 @@ class QuoteListItem extends StatelessWidget {
                                       child: RepaintBoundary(
                                     key: _repaintBoundaryKey,
                                     child: QuoteBody(
-                                      quote: quote,
+                                      quote: widget.quote,
                                     ),
                                   )),
                                 ),
@@ -271,5 +283,24 @@ class QuoteListItem extends StatelessWidget {
         ),
       ),
     );
+
+    if (_shareDebugMode) {
+      return Stack(
+        children: [
+          listItem,
+          Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blueAccent, width: 3)
+              ),
+              width: 300,
+              height: 300,
+              child: _memoryImage != null
+                  ? Image.memory(_memoryImage)
+                  : Placeholder()),
+        ],
+      );
+    } else {
+      return listItem;
+    }
   }
 }
