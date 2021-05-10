1b56295023d92f1e65524325a5d9d22ff455ebef
Carlos Pinelly <cpinelly@gmail.com>
2021-05-10 18:37:15 -0500

    Added new top bar base.

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -6,6 +6,89 @@ import 'package:quotesbook/screens/favorites_list.dart';
 import 'package:quotesbook/screens/quotes_list.dart';
 import 'package:quotesbook/widgets/bookmark.dart';
 
+class AppBarBottomDecorationPainter extends CustomPainter {
+  Color color;
+
+  AppBarBottomDecorationPainter({this.color = Colors.blue});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = color;
+    var path = Path();
+
+    path.moveTo(size.width / 2, 0);
+    path.lineTo(size.width, size.height / 2);
+    path.lineTo(size.width / 2, size.height);
+    path.lineTo(0, size.height / 2);
+
+    path.close();
+
+    var paint2 = Paint();
+    paint2.color = Colors.black.withOpacity(0.1);
+
+    canvas.drawLine(Offset(0, (size.height / 2) + 1),
+        Offset(size.width, (size.height / 2) + 1), paint2);
+    canvas.drawShadow(path, Colors.black, 2, false);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class CustomTopbar extends StatelessWidget {
+  String title;
+  Color color;
+  double margin;
+
+  static const double HEIGHT = 70;
+  static const double SPIKE_HEIGHT = 10;
+
+  CustomTopbar({this.title, this.color, this.margin = 25});
+
+  @override
+  Widget build(BuildContext context) {
+    var diamondMargin = margin - 8;
+    return Container(
+      width: double.infinity,
+      height: HEIGHT,
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) => Stack(
+          children: [
+            Positioned(
+              bottom: 0,
+              height: SPIKE_HEIGHT,
+              left: diamondMargin,
+              width: constraints.maxWidth - (diamondMargin * 2),
+              child: CustomPaint(
+                painter: AppBarBottomDecorationPainter(color: color),
+              ),
+            ),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  color: color,
+                  height: HEIGHT - (SPIKE_HEIGHT / 2),
+                ),
+                Text(
+                  title.toUpperCase(),
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class TabsScreen extends StatefulWidget {
   var lang = 'en';
 
@@ -66,17 +149,34 @@ class _TabsScreenState extends State<TabsScreen>
   @override
   Widget build(BuildContext context) {
     Provider.of<SavedQuotes>(context, listen: false).loadSavedQuotes();
-    var localizations = AppLocalizations.of(context);
+    final screenSize = MediaQuery.of(context).size;
+    final listHeight = screenSize.height - CustomTopbar.HEIGHT + (CustomTopbar.SPIKE_HEIGHT / 2);
 
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
       body: SafeArea(
         child: Stack(
           children: [
-            PageView(
-              physics: NeverScrollableScrollPhysics(),
-              children: _pages.map<Widget>((i) => i['page']).toList(),
-              controller: _pageController,
+            Column(
+              children: [
+                // I don't use a Positioned widget because throws some
+                // "RenderBox was not laid out" error
+                SizedBox(height: screenSize.height - listHeight,),
+                Expanded(
+                  child: PageView(
+
+                    physics: NeverScrollableScrollPhysics(),
+                    children: _pages.map<Widget>((i) => i['page']).toList(),
+                    controller: _pageController,
+                  ),
+                ),
+              ],
+            ),
+
+            CustomTopbar(
+              title: "home",
+              color: Colors.grey.shade300,
+              margin: 25,
             ),
             Positioned(
               right: 22,
@@ -92,18 +192,6 @@ class _TabsScreenState extends State<TabsScreen>
                             ? Colors.amber
                             : Colors.black12),
                       ),
-                      /*
-                                if (quote.isFavorite)
-                                  Container(
-                                    child: Center(
-                                      child: Icon(
-                                        Icons.star,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                    width: 50,
-                                    height: 40,
-                                  ) */
                     ],
                   ),
                   onTap: _onTabSelected),
