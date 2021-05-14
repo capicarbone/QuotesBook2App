bafcfd2306fabc0b32d79b51ecfa1fe7dd2bb9df
Carlos Pinelly <cpinelly@gmail.com>
2021-05-14 18:13:27 -0500

    moved topbar code to separated file.

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -5,89 +5,7 @@ import 'package:quotesbook/providers/saved_quotes.dart';
 import 'package:quotesbook/screens/favorites_list.dart';
 import 'package:quotesbook/screens/quotes_list.dart';
 import 'package:quotesbook/widgets/bookmark.dart';
-
-class AppBarBottomDecorationPainter extends CustomPainter {
-  Color color;
-
-  AppBarBottomDecorationPainter({this.color = Colors.blue});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    var paint = Paint();
-    paint.color = color;
-    var path = Path();
-
-    path.moveTo(size.width / 2, 0);
-    path.lineTo(size.width, size.height / 2);
-    path.lineTo(size.width / 2, size.height);
-    path.lineTo(0, size.height / 2);
-
-    path.close();
-
-    //var paint2 = Paint();
-    //paint2.color = Colors.black.withOpacity(0.1);
-
-   // canvas.drawLine(Offset(0, (size.height / 2) + 1),
-        //Offset(size.width, (size.height / 2) + 1), paint2);
-    canvas.drawShadow(path, Colors.black, 2, false);
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
-  }
-}
-
-class CustomTopbar extends StatelessWidget {
-  String title;
-  Color color;
-  double margin;
-
-  static const double HEIGHT = 70;
-  static const double SPIKE_HEIGHT = 7;
-
-  CustomTopbar({this.title, this.color, this.margin = 25});
-
-  @override
-  Widget build(BuildContext context) {
-    var diamondMargin = 5.0;
-    return Container(
-      width: double.infinity,
-      height: HEIGHT,
-      child: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) => Stack(
-          children: [
-            Positioned(
-              bottom: 0,
-              height: SPIKE_HEIGHT,
-              left: diamondMargin,
-              width: constraints.maxWidth - (diamondMargin * 2),
-              child: CustomPaint(
-                painter: AppBarBottomDecorationPainter(color: color),
-              ),
-            ),
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                Container(
-                  color: color,
-                  height: HEIGHT - (SPIKE_HEIGHT / 2),
-                ),
-                Text(
-                  title.toUpperCase(),
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                )
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+import 'package:quotesbook/widgets/topbar.dart';
 
 class TabsScreen extends StatefulWidget {
   var lang = 'en';
@@ -150,7 +68,7 @@ class _TabsScreenState extends State<TabsScreen>
   Widget build(BuildContext context) {
     Provider.of<SavedQuotes>(context, listen: false).loadSavedQuotes();
     final screenSize = MediaQuery.of(context).size;
-    final listHeight = screenSize.height - CustomTopbar.HEIGHT + (CustomTopbar.SPIKE_HEIGHT / 2);
+    final listHeight = screenSize.height - Topbar.HEIGHT + (Topbar.SPIKE_HEIGHT / 2);
 
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
@@ -173,7 +91,7 @@ class _TabsScreenState extends State<TabsScreen>
               ],
             ),
 
-            CustomTopbar(
+            Topbar(
               title: "home",
               color: Colors.grey.shade300,
               margin: 25,
@@ -187,7 +105,7 @@ class _TabsScreenState extends State<TabsScreen>
                         duration: Duration(milliseconds: 300),
                         width: 70,
                         curve: Curves.bounceOut,
-                        height: _selectedPageIndex == 1 ? CustomTopbar.HEIGHT - 10 : CustomTopbar.HEIGHT - 30,
+                        height: _selectedPageIndex == 1 ? Topbar.HEIGHT - 10 : Topbar.HEIGHT - 30,
                         child: Bookmark(_selectedPageIndex == 1
                             ? Theme.of(context).accentColor
                             : Colors.black12),
---
+++ lib/widgets/topbar.dart
@@ -0,0 +1,85 @@
+
+import 'package:flutter/material.dart';
+
+class AppBarBottomDecorationPainter extends CustomPainter {
+  Color color;
+
+  AppBarBottomDecorationPainter({this.color = Colors.blue});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = color;
+    var path = Path();
+
+    path.moveTo(size.width / 2, 0);
+    path.lineTo(size.width, size.height / 2);
+    path.lineTo(size.width / 2, size.height);
+    path.lineTo(0, size.height / 2);
+
+    path.close();
+
+    //var paint2 = Paint();
+    //paint2.color = Colors.black.withOpacity(0.1);
+
+    // canvas.drawLine(Offset(0, (size.height / 2) + 1),
+    //Offset(size.width, (size.height / 2) + 1), paint2);
+    canvas.drawShadow(path, Colors.black, 2, false);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class Topbar extends StatelessWidget {
+  String title;
+  Color color;
+  double margin;
+
+  static const double HEIGHT = 70;
+  static const double SPIKE_HEIGHT = 7;
+
+  Topbar({this.title, this.color, this.margin = 25});
+
+  @override
+  Widget build(BuildContext context) {
+    var diamondMargin = 5.0;
+    return Container(
+      width: double.infinity,
+      height: HEIGHT,
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) => Stack(
+          children: [
+            Positioned(
+              bottom: 0,
+              height: SPIKE_HEIGHT,
+              left: diamondMargin,
+              width: constraints.maxWidth - (diamondMargin * 2),
+              child: CustomPaint(
+                painter: AppBarBottomDecorationPainter(color: color),
+              ),
+            ),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  color: color,
+                  height: HEIGHT - (SPIKE_HEIGHT / 2),
+                ),
+                Text(
+                  title.toUpperCase(),
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
