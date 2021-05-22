fb319ee34ef832f0f983a1c30bff1063b52f7dc3
capi <cpinelly@gmail.com>
2021-05-22 18:58:19 -0500

    Fixing clipped large words.

--- lib/widgets/quote_body.dart
+++ lib/widgets/quote_body.dart
@@ -1,8 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:quotesbook/models/Quote.dart';
-
 
 class QuoteText extends StatelessWidget {
   double minFontSize;
@@ -12,9 +10,9 @@ class QuoteText extends StatelessWidget {
 
   QuoteText(
       {this.text,
-        this.constraints,
-        this.minFontSize = 12,
-        this.align = TextAlign.center});
+      this.constraints,
+      this.minFontSize = 12,
+      this.align = TextAlign.center});
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +26,7 @@ class QuoteText extends StatelessWidget {
         minWidth: constraints.minWidth, maxWidth: constraints.maxWidth);
 
     var adjusted = false;
+    final desirableFontSize = text.style.fontSize;
 
     // If the default font size surpasses the constraints limit then it test to
     // reduces the font size in loop until we get a valid value or a minimum
@@ -39,9 +38,9 @@ class QuoteText extends StatelessWidget {
       if (!adjusted) {
         var currentFontSize = painter.text.style.fontSize;
         var nextFontSize =
-        ((currentFontSize - minFontSize) * 0.70 + minFontSize)
-            .floor()
-            .toDouble();
+            ((currentFontSize - minFontSize) * 0.70 + minFontSize)
+                .floor()
+                .toDouble();
         var newText = TextSpan(
             text: text.text,
             style: text.style.copyWith(fontSize: nextFontSize));
@@ -50,6 +49,27 @@ class QuoteText extends StatelessWidget {
             minWidth: constraints.minWidth, maxWidth: constraints.maxWidth);
       }
     } while (!adjusted && painter.text.style.fontSize > minFontSize);
+
+    final sortedWords = text.text.split(" ")
+      ..sort((a, b) => b.length.compareTo(a.length));
+    final largestWordLength = sortedWords[0].length;
+
+    if (largestWordLength > 6) {
+      final measuredFontSize = painter.text.style.fontSize;
+      // The constant in the adjustment are calculated as a line equation (mx + b)
+      // using (font size calculated, min font size for correct adjustment) => (50, 12), (38, 12)
+      // as points for determine the constants.
+      // This is not a definitive solution for clipped large words yet, but has
+      // decreased considerably the number of occurrences.
+      final fontSizeAdjustment = (0.583 * measuredFontSize - 17.17);
+      painter.text = TextSpan(
+          text: text.text,
+          style: painter.text.style.copyWith(
+              // the constant for
+              fontSize: painter.text.style.fontSize - fontSizeAdjustment));
+      painter.layout(
+          minWidth: constraints.minWidth, maxWidth: constraints.maxWidth);
+    }
 
     return SizedBox(
       width: painter.size.width,
@@ -64,7 +84,6 @@ class QuoteText extends StatelessWidget {
 }
 
 class QuoteBody extends StatelessWidget {
-
   Quote quote;
   var quoteFontSize;
 
@@ -72,26 +91,25 @@ class QuoteBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     var textColor = Theme.of(context).primaryColor;
 
-    return LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
-      var quoteConstraints = constraints.copyWith(maxHeight: constraints.maxHeight*0.7);
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      var quoteConstraints =
+          constraints.copyWith(maxHeight: constraints.maxHeight * 0.7);
       return Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           QuoteText(
-                  constraints: quoteConstraints,
-                  text: TextSpan(
-                      text: quote.body,
-                      style: GoogleFonts.montserrat(
-                          textStyle: TextStyle(
-                              fontSize: quoteFontSize,
-                              fontWeight: FontWeight.bold,
-                              color: textColor))
-                  )
-              ),
+              constraints: quoteConstraints,
+              text: TextSpan(
+                  text: quote.body,
+                  style: GoogleFonts.montserrat(
+                      textStyle: TextStyle(
+                          fontSize: quoteFontSize,
+                          fontWeight: FontWeight.bold,
+                          color: textColor)))),
           SizedBox(
             height: 25,
           ),
@@ -111,9 +129,7 @@ class QuoteBody extends StatelessWidget {
                 Text(
                   '${quote.author.firstName} ${quote.author.lastName}',
                   style: GoogleFonts.montserrat(
-                      textStyle: TextStyle(
-                          fontSize: 15,
-                          color: textColor)),
+                      textStyle: TextStyle(fontSize: 15, color: textColor)),
                   textAlign: TextAlign.center,
                 ),
               ],
